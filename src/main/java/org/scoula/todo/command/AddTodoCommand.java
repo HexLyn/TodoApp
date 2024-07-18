ab2ffb4195d2a6fa53ca4a13d19edc40f678c4f5
KIMKANGMIN <rkdals159@naver.com>
2024-07-18 17:47:00 +0900

    success

--- src/main/java/org/scoula/todo/command/AddTodoCommand.java
+++ src/main/java/org/scoula/todo/command/AddTodoCommand.java
@@ -16,12 +16,19 @@ public class AddTodoCommand implements Command {
         String description = Input.getLine("설명 : ", "default");
         System.out.println("새 Todo 추가 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 
+        // Todo.builder(). ~~~ . build();
+        // 사용자에게 받아온 제목과 설명으로 새로운 할 일 생성.
         Todo todo = Todo.builder()
                 .title(title)
                 .description(description)
                 .done(false)
                 .build();
+        // 생성된 것을 할일 목록에 추가한다.
         dao.add(todo);
+        // dao는 TodoDao dao = TodoListDao.getInstance(); 이며
+        // getInstance는 instance를 반환하고,
+        // private static TodoListDao instance = new TodoListDao();
+        // instance는 TodoListDao로
 
         System.out.println();
     }
